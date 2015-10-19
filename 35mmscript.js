bdca54f58868c94bd6f258994cc30c937aef8531
Sean Delanty <Sean@Seans-MacBook-Pro.local>
2015-10-19 17:55:00 -0400

    added 35mm and iphone photo sections

---
+++ 35mmscript.js
@@ -0,0 +1,17 @@
+var usedImages = {};
+var usedImagesCount = 0;
+var numImages = 35;
+function displayImage() {
+    var num = 1+Math.floor(Math.random() * (numImages));
+    if (!usedImages[num]){
+        document.canvas.src = 'images/35mm/'+num+'.jpg';
+        usedImages[num] = true;
+        usedImagesCount++;
+        if (usedImagesCount == numImages) {
+            usedImagesCount = 0;
+            usedImages = {};
+        }
+    } else {
+        displayImage();
+    }
+}
